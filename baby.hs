f28ea34b6684f5c42e51bf7c59b91f914adf3735
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-10-21 19:52:52 -0700

    Try two different implementations of factorial

--- baby.hs
+++ baby.hs
@@ -6,3 +6,10 @@ doubleSmallerNumber x = if x > 100
                           then x
                           else x*2
 
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = product [1..n]
+
+factorialRecursive :: (Integral a) => a -> a
+factorialRecursive 0  = 1
+factorialRecursive n = n * factorialRecursive (n - 1)
