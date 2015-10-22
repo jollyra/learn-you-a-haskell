1f3b98d5116894e5b140db3fa52b9489229d9142
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-10-21 23:38:06 -0700

    Complete vector excercise
    Includes outline of Class Constraints and args.

--- baby.hs
+++ baby.hs
@@ -6,6 +6,7 @@ doubleSmallerNumber x = if x > 100
                           then x
                           else x*2
 
+-- Factorial
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = product [1..n]
@@ -13,3 +14,11 @@ factorial n = product [1..n]
 factorialRecursive :: (Integral a) => a -> a
 factorialRecursive 0  = 1
 factorialRecursive n = n * factorialRecursive (n - 1)
+
+-- Adding vectors
+-----------------
+-- Class Constraint :: arg -> arg -> return
+-- Args must obey the class contraint
+-- Class contraint is a Typeclass
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
