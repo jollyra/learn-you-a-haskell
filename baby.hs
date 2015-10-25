58fb7a6c73bc1a28162032d4494971f22c091770
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-10-24 18:17:24 -0700

    Fibonacci calculator

--- baby.hs
+++ baby.hs
@@ -1,5 +1,4 @@
 doubleMe x = x + x
-
 doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallerNumber x = if x > 100
@@ -22,3 +21,22 @@ factorialRecursive n = n * factorialRecursive (n - 1)
 -- Class contraint is a Typeclass
 addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)
 addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+-- Fibonacci calculator
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib x = fib (x - 1) + fib (x - 2)
+
+-- Bracket matching interview question
+-- Test cases:
+-- (())
+-- ()()
+-- (()
+-- )(
+brack :: [Char] -> Bool
+brack [] = True
+brack [x] = False
+brack (a:b:xs) = case a of ')' -> False
+                           '(' -> if b == ')' then brack xs
+                                              else brack (b:xs)
