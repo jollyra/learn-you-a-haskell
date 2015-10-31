9eb02eab457ba8164ebf668e45bb46b06bc051f2
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-10-31 12:34:04 -0700

    Try variation of quicksort using filter

--- quicksort.hs
+++ quicksort.hs
@@ -4,3 +4,8 @@ quicksort (x:xs) =
   let smallerSorted = quicksort [a | a <- xs, a <= x]
       biggerSorted = quicksort [a | a <- xs, a > x]
   in smallerSorted ++ [x] ++ biggerSorted
+
+-- A variation of quicksort using filter instead of list comprehensions
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x:xs) = quicksort' (filter (<x) xs) ++ [x] ++ quicksort' (filter (>=x) xs)
