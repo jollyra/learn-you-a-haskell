9a247b0f361be6c0c85e6e8d64fbe1a7fd9ef721
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-11-04 07:55:04 -0800

    Use a folding function for stack

--- bracketmatch.hs
+++ bracketmatch.hs
@@ -2,4 +2,7 @@ sum' :: (Num a) => [a] -> a
 sum' xs = foldl (\acc x -> acc + x) 0 xs
 
 stackTest :: (Num a) => [a] -> [a]
-stackTest xs = foldr (\x acc -> x : acc) [] xs
+stackTest xs = foldr foldingFun [] xs
+
+foldingFun :: (Num a) => a -> [a] -> [a]
+foldingFun y xs = y : xs
