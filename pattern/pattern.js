5d911bfbba1353f994f6ce48d649422aa3d39e7f
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-12-12 13:25:33 -0800

    Limit scope and use canvas object

--- pattern/pattern.js
+++ pattern/pattern.js
@@ -1,11 +1,22 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+(function () {
+	var canvas = {
+		ctx: $("#myCanvas").get(0).getContext("2d"),
 
-// Create gradient
-var grd = ctx.createRadialGradient(75,50,5,90,60,100);
-grd.addColorStop(0,"red");
-grd.addColorStop(1,"white");
+		fillPixel: function fillPixel(x, y, colour) {
+			ctx.fillStyle = "#FF0000";
+		},
 
-// Fill with gradient
-ctx.fillStyle = grd;
-ctx.fillRect(10,10,800,800);
+		drawGrad: function () {
+			var grd = this.ctx.createRadialGradient(75,50,5,90,60,100);
+			grd.addColorStop(0,"red");
+			grd.addColorStop(1,"white");
+
+			// Fill with gradient
+			var viewWidth = $(window).width();
+			this.ctx.fillStyle = grd;
+			this.ctx.fillRect(0,0,viewWidth,viewWidth);
+		}
+	};
+
+	canvas.drawGrad();
+}) ()
