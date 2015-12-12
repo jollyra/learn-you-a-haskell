14d4328067d2992b8fad531bc18a44b1b237434e
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-12-12 13:45:01 -0800

    Use a stick of tiles as underlying map data struct

--- pattern/pattern.js
+++ pattern/pattern.js
@@ -6,6 +6,10 @@
 			ctx.fillStyle = "#FF0000";
 		},
 
+		fillStick: function fillStick(stick, size) {
+
+		},
+
 		drawGrad: function () {
 			var grd = this.ctx.createRadialGradient(75,50,5,90,60,100);
 			grd.addColorStop(0,"red");
@@ -17,6 +21,21 @@
 			this.ctx.fillRect(0,0,viewWidth,viewWidth);
 		}
 	};
+
+	var map = {
+		init: function init(size) {
+			this.stick = []
+			for(int i = 0; i < size * size; i++) {
+				this.stick.push(0);
+			}
+		},
+
+		getTile: function getTile(x, y) {
+			var tileIdx = y * map.size + x;
+			assert.equal(tileIdx < this.stick.tiles.length, true, 'coordinates out of bounds');
+			return this.stick.tiles[tileIdx];
+		}
+	}
 
 	canvas.drawGrad();
 }) ()
