77b152f5a86b74e7637123133a94543bc7f1cfdb
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2015-12-12 14:12:56 -0800

    Create a pattern on the map data structure

--- pattern/pattern.js
+++ pattern/pattern.js
@@ -3,24 +3,15 @@
 		ctx: $("#myCanvas").get(0).getContext("2d"),
 
 		fillPixel: function fillPixel(x, y, colour) {
+			console.log(arguments);
 			this.ctx.fillStyle = colour;
 			this.ctx.fillRect(x, y, 1, 1);
-		},
-
-		drawGrad: function () {
-			var grd = this.ctx.createRadialGradient(75,50,5,90,60,100);
-			grd.addColorStop(0,"red");
-			grd.addColorStop(1,"white");
-
-			// Fill with gradient
-			var viewWidth = $(window).width();
-			this.ctx.fillStyle = grd;
-			this.ctx.fillRect(0, 0, viewWidth, viewWidth);
 		}
 	};
 
 	var map = {
 		init: function init(size) {
+			this.size = size;
 			this.stick = []
 			for(var i = 0; i < size * size; i++) {
 				this.stick.push(0);
@@ -29,19 +20,41 @@
 
 		getTile: function getTile(x, y) {
 			var tileIdx = y * map.size + x;
-			assert.equal(tileIdx < this.stick.tiles.length, true, 'coordinates out of bounds');
-			return this.stick.tiles[tileIdx];
+			return this.stick[tileIdx];
 		},
 		
 		setTile: function getTile(x, y, val) {
 			var tileIdx = y * map.size + x;
-			assert.equal(tileIdx < this.stick.tiles.length, true, 'coordinates out of bounds');
-			return this.stick.tiles[tileIdx] = val;
+			return this.stick[tileIdx] = val;
 		}
 	}
 
+	function drawPattern (map, canvas) {
+		for(var y = 0; y < map.size; y++) {
+			for(var x = 0; x < map.size; x++) {
+				if ((y * x) % 2 == 0) {
+					map.setTile(x, y, 1);
+				} else {
+					map.setTile(x, y, 0);
+				}
+			}
+		}
+
+		for(var y = 0; y < map.size; y++) {
+			for(var x = 0; x < map.size; x++) {
+				var val = map.getTile(x,y);
+				var colour;
+				if (val === 1) {
+					colour = "F00000";
+				} else if (val ===0) {
+					colour = "0000FF";
+				}
+				canvas.fillPixel(x, y, colour);
+			}
+		}
+	}
 
 	var viewWidth = $(window).width();
 	map.init(viewWidth);
-	canvas.drawGrad();
+	drawPattern(map, canvas);
 }) ()
