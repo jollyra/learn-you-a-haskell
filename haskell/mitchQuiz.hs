190ffdec7d7f1a8a656585cf6b662e01d7dc6f77
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-02-18 18:04:41 -0800

    Starting splitWith

--- haskell/mitchQuiz.hs
+++ haskell/mitchQuiz.hs
@@ -1,3 +1,10 @@
 join :: [a] -> [a] -> [a]
 join [] ys = ys
 join xs ys = head xs : join (tail xs) ys
+
+-- [2,2,2,1,2,4,7,8]
+-- [[2,2,2],[2,4],[8]]
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith f xs = foldr splitOrStay [] xs
+                            where splitOrStay acc (x:xs)
+                                      | (f x) == True
