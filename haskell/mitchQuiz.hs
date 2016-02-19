267719ac87f36090f4f71100fbe8e156fe1b2e4a
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-02-19 13:46:47 -0800

    Define two inner function

--- haskell/mitchQuiz.hs
+++ haskell/mitchQuiz.hs
@@ -2,9 +2,11 @@ join :: [a] -> [a] -> [a]
 join [] ys = ys
 join xs ys = head xs : join (tail xs) ys
 
--- [2,2,2,1,2,4,7,8]
+la = [2,2,2,1,2,4,7,8]
 -- [[2,2,2],[2,4],[8]]
 splitWith :: (a -> Bool) -> [a] -> [[a]]
-splitWith f xs = foldr splitOrStay [] xs
-                            where splitOrStay acc (x:xs)
-                                      | (f x) == True
+-- splitWith :: (a -> Bool) -> [a] -> [a]
+splitWith f [] = []
+splitWith f xs = splitWith' f xs acc
+    where splitWith' f xs [] 
+        takeTo = undefined
