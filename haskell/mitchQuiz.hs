e027203871948e47fe8f6bc3cc5ddfa4570b68c0
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-02-20 15:08:06 -0800

    Delete horrible try

--- haskell/mitchQuiz.hs
+++ haskell/mitchQuiz.hs
@@ -4,9 +4,7 @@ join xs ys = head xs : join (tail xs) ys
 
 la = [2,2,2,1,2,4,7,8]
 -- [[2,2,2],[2,4],[8]]
+
 splitWith :: (a -> Bool) -> [a] -> [[a]]
--- splitWith :: (a -> Bool) -> [a] -> [a]
-splitWith f [] = []
-splitWith f xs = splitWith' f xs acc
-    where splitWith' f xs [] 
-        takeTo = undefined
+splitWith f xs = (takeWhile f xs) : splitWith (dropWhile f xs)
+
