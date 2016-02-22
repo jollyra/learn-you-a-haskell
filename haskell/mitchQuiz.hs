0d21e9869198c70e4b7714afc88bd822c3992d73
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-02-21 18:09:22 -0800

    SplitWith implemented with no standard lib

--- haskell/mitchQuiz.hs
+++ haskell/mitchQuiz.hs
@@ -2,9 +2,28 @@ join :: [a] -> [a] -> [a]
 join [] ys = ys
 join xs ys = head xs : join (tail xs) ys
 
+
+
 la = [2,2,2,1,2,4,7,8]
 -- [[2,2,2],[2,4],[8]]
 
-splitWith :: (a -> Bool) -> [a] -> [[a]]
-splitWith f xs = (takeWhile f xs) : splitWith (dropWhile f xs)
+splitWith :: (a -> Bool) -> [a] ->[[a]]
+splitWith f [] = []
+splitWith f xs
+            | null (fst chunk) = (splitWith f (snd chunk))
+            | otherwise        = (fst chunk) : (splitWith f (snd chunk))
+    where chunk = splitWhen f xs
 
+
+splitWhen :: (a -> Bool) -> [a] -> ([a], [a])
+splitWhen f xs = (takeUntil f xs, dropUntil f xs)
+
+
+takeUntil f xs = takeWhile (not . f) xs
+
+
+dropUntil :: (a -> Bool) -> [a] -> [a]
+dropUntil _ [] = []
+dropUntil f (x:xs)
+              | f x == True = xs
+              | otherwise   = dropUntil f xs
