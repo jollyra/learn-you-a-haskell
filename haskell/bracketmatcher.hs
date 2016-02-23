5f3d5d881123f72fe03ef747a2739d3da663dfaa
Nigel Rahkola <Nigel.Rahkola@enernoc.com>
2016-02-22 22:43:19 -0800

    Better solution to bracketmatcher

--- haskell/bracketmatcher.hs
+++ haskell/bracketmatcher.hs
@@ -1,10 +1,26 @@
-validate :: String -> Bool
-validate xs = length validatedExpression == 0
-        where validatedExpression = foldl foldingFunction [] xs
+-- (())
+-- ()()
+-- )(
+-- (()
+-- ())
 
-foldingFunction stack '(' = stack ++ "("
-foldingFunction stack ')' = case stack of
-                            ('(':xs) -> init stack
-                            (')':xs) -> stack
-                            ([]) -> stack
-foldingFunction stack _ = stack
+--                Input     Stack     Result
+bracketmatcher :: String -> String -> Bool
+bracketmatcher [] []     = True
+bracketmatcher [] stack
+                    | not $ null stack = False
+bracketmatcher (x:xs) []
+                    | x == '(' = bracketmatcher xs (x:[])
+                    | x == ')' = False
+                    | otherwise = bracketmatcher xs []
+bracketmatcher (x:xs) (y:ys)
+                    | x == '(' = bracketmatcher xs (x:y:ys)
+                    | x == ')' = (popAndCompare (y:ys)) && (bracketmatcher xs ys)
+                    | otherwise = bracketmatcher xs (y:ys)
+
+
+popAndCompare :: String -> Bool
+popAndCompare [] = False
+popAndCompare (x:__)
+                | x == '('  = True
+                | otherwise = False
