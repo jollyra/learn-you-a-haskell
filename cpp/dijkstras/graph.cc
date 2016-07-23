04ac3519759f86fea72c04ae5d530225d829332f
Nigel Rahkola <me@nigelrahkola.com>
2016-07-22 22:08:12 -0700

    Use a matrix instead of adjacency list

---
+++ cpp/dijkstras/graph.cc
@@ -0,0 +1,46 @@
+/* Graph data type using list representation
+ */
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Graph {
+    public:
+        Graph() {}
+
+        Graph(int size) {
+            for(int i = 0; i < size; i++) {
+                graph_.push_back(vector<int> (size, -1));
+            }
+        }
+
+        ~Graph() {}
+
+        int V() {
+            int count = 0;
+            for(int i = 0; i < graph_.size(); i++) {
+                for(int k = 0; k < graph_.size(); k++) {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        void add_edge() {
+
+        }
+
+        void tostring() {
+
+        }
+
+    private:
+        vector<vector<int> > graph_;
+};
+
+int main() {
+    Graph g = Graph(3);
+    cout << "size: " << g.V() << endl;
+    return 0;
+}
